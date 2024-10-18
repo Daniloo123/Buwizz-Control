8ee7a0da1c98a596bdfaf2f55f4a5d7af18b208d
16085310 <danilo_isabela@hotmail.com>
2024-10-18 16:57:38 +0200

    Failure 2

--- BuwizzControl.py
+++ BuwizzControl.py
@@ -1,8 +1,7 @@
-from bleak import BleakClient
+bleak import BleakClient
 import asyncio
 from random import randint
 from time import sleep
-import numpy as np
 #50058000-74fb-4481-88b3-9919b1676e93
 
 address = "D8:36:DC:FD:9C:F0"
@@ -20,16 +19,20 @@ async def control_buwizz():
 
                 # Houd de verbinding open
                 while True:
-                    
-                    for i in range(10):
-                        CHARACTERISTIC_UUID = "936e2901-1999-b388-8144fb74d1920550" #uit de api docs gehaald door mar
-                        CHARACTERISTIC_UUID = "50058000-74fb-4481-88b3-9919b1676e93" #uit de buwizz gehaald
-                        CHARACTERISTIC_UUID = "50052901-74fb-4481-88b3-9919b1676e93" #aangepast door mar
-                        LED_STATUS = randint(1, 16)
-                        nparr = np.array([36, LED_STATUS])
-                        data = await client.write_gatt_char(CHARACTERISTIC_UUID, nparr.tobytes())
-                        sleep(0.5)
-                        exit()
+                    command = input("Voer een commando in of typ 'exit' om de verbinding te verbreken: ")
+                    if command.lower() == 'exit':
+                        print("Verbinding verbreken...")
+                        break
+                    elif command.lower() == '1':
+                        #Motor 1 met snelheid 100 laten draaien
+                        motor_1_command = bytearray([0x10, 50, 176, 0, 0, 0x00])
+                        await client.write_gatt_char(characteristic_uuid, motor_1_command)
+                        print("Motor 1 commando verzonden")
+                    elif command.lower() == 'stop':
+                        stop_command = bytearray([0x10, 0, 0, 0, 0, 0x00])
+                        await client.write_gatt_char(characteristic_uuid, stop_command)
+                        print("Alle motoren gestopt")
+
             except Exception as e:
                 print(f"Fout bij het verzenden van het commando: {e}")
         else:
